8e634b69786ed0e5ce856c828104fa34dcad0564
siddheshparadkar83 <siddheshparadkar83@gmail.com>
2025-09-27 01:12:54 +0530

    extent report

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,14 +4,34 @@ import java.lang.System.Logger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+	
+	@AfterSuite
+	public void teardownReport() {
+		extent.flush();
+	}
 	
 	@BeforeMethod
 	public void setUp() {
@@ -26,7 +46,16 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "Login Failure");
+			test.fail("Test Failed....check screenshot",
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+		
+		
 		if(driver != null) {
 			Log.info("Closing browser");
 			driver.quit();
--- src/main/java/test/LoginTest.java
+++ src/main/java/test/LoginTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest{
@@ -13,16 +14,50 @@ public class LoginTest extends BaseTest{
 	public void testValidLogin() {
 		
 		Log.info("staring test");
+		test = ExtentReportManager.createTest("Login test");
+		
+		test.info("navigating to URL");
 		LoginPage loginPage = new LoginPage(driver);
 		
+		test.info("Adding Creds..");
 		Log.info("entering creds..");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
+		
+		test.info("Clicking Login Button");
 		loginPage.clickLogin();
 		System.out.println("The title of the page is:" + driver.getTitle());
 		
 		Log.info("Verifying title");
+		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
+		
+		test.pass("Login Successful");
+	}
+	
+	@Test
+	public void testInvalidLogin() {
+		
+		Log.info("staring test");
+		test = ExtentReportManager.createTest("Login test");
+		
+		test.info("navigating to URL");
+		LoginPage loginPage = new LoginPage(driver);
+		
+		test.info("Adding Creds..");
+		Log.info("entering creds..");
+		loginPage.enterUsername("admin123@yourstore.com");
+		loginPage.enterPassword("admin123");
+		
+		test.info("Clicking Login Button");
+		loginPage.clickLogin();
+		System.out.println("The title of the page is:" + driver.getTitle());
+		
+		Log.info("Verifying title");
+		test.info("Verifying page title");
+		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration123");
+		
+		test.pass("Login Successful");
 	}
 
 }
