3ce8153b86091cd4dd9e7ca785f3d9b7618f12d1
siddheshparadkar83 <siddheshparadkar83@gmail.com>
2025-09-28 17:34:14 +0530

    Data Driven Test

--- src/main/java/test/LoginTest.java
+++ src/main/java/test/LoginTest.java
@@ -1,63 +1,98 @@
 package test;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
 import utils.ExtentReportManager;
 import utils.Log;
 
-public class LoginTest extends BaseTest{
-	
+public class LoginTest extends BaseTest {
+
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData() throws IOException {
+
+		String filePath = System.getProperty("user.dir") + "/testdata/TestData.xlsx";
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // password
+		}
+
+		ExcelUtils.closeExcel();
+		return data;
+	}
+
+	@DataProvider(name = "LoginData2")
+	public Object[][] getData() {
+		return new Object[][] { { "user1", "pass1" }, { "user2", "pass2" }, { "user3", "pass3" }
+
+		};
+	}
+
+	//@Test(dataProvider = "LoginData2")
 	@Test
-	public void testValidLogin() {
-		
+	@Parameters({"username","password"})
+	public void testValidLogin(String username, String password) {
+
 		Log.info("staring test");
-		test = ExtentReportManager.createTest("Login test");
-		
+		test = ExtentReportManager.createTest("Login test -" + username);
+
 		test.info("navigating to URL");
 		LoginPage loginPage = new LoginPage(driver);
-		
+
 		test.info("Adding Creds..");
 		Log.info("entering creds..");
-		loginPage.enterUsername("admin@yourstore.com");
-		loginPage.enterPassword("admin");
-		
+
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
+
+		/*
+		 * loginPage.enterUsername("admin@yourstore.com");
+		 * loginPage.enterPassword("admin");
+		 */
 		test.info("Clicking Login Button");
 		loginPage.clickLogin();
 		System.out.println("The title of the page is:" + driver.getTitle());
-		
+
 		Log.info("Verifying title");
 		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
-		
-		test.pass("Login Successful");
-	}
-	
-	@Test
-	public void testInvalidLogin() {
-		
-		Log.info("staring test");
-		test = ExtentReportManager.createTest("Login test");
-		
-		test.info("navigating to URL");
-		LoginPage loginPage = new LoginPage(driver);
-		
-		test.info("Adding Creds..");
-		Log.info("entering creds..");
-		loginPage.enterUsername("admin123@yourstore.com");
-		loginPage.enterPassword("admin123");
-		
-		test.info("Clicking Login Button");
-		loginPage.clickLogin();
-		System.out.println("The title of the page is:" + driver.getTitle());
-		
-		Log.info("Verifying title");
-		test.info("Verifying page title");
-		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration123");
-		
+
 		test.pass("Login Successful");
 	}
 
+	/*
+	 * @Test public void testInvalidLogin() {
+	 * 
+	 * Log.info("staring test"); test =
+	 * ExtentReportManager.createTest("Login test");
+	 * 
+	 * test.info("navigating to URL"); LoginPage loginPage = new LoginPage(driver);
+	 * 
+	 * test.info("Adding Creds.."); Log.info("entering creds..");
+	 * loginPage.enterUsername("admin123@yourstore.com");
+	 * loginPage.enterPassword("admin123");
+	 * 
+	 * test.info("Clicking Login Button"); loginPage.clickLogin();
+	 * System.out.println("The title of the page is:" + driver.getTitle());
+	 * 
+	 * Log.info("Verifying title"); test.info("Verifying page title");
+	 * Assert.assertEquals(driver.getTitle(),
+	 * "Dashboard / nopCommerce administration123");
+	 * 
+	 * test.pass("Login Successful"); }
+	 */
 }
---
+++ src/main/java/utils/ExcelUtils.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+
+	private static Workbook workbook;
+	private static Sheet sheet;
+
+	// load excel file
+
+	public static void loadExcel(String filePath, String sheetName) throws IOException {
+		FileInputStream file = new FileInputStream(filePath);
+		workbook = new XSSFWorkbook(file);
+		sheet = workbook.getSheet(sheetName);
+	}
+
+	// get cell data as string
+
+	public static String getCellData(int row, int col) {
+		Cell cell = sheet.getRow(row).getCell(col);
+
+		if (cell.getCellType() == CellType.STRING) {
+			return cell.getStringCellValue();
+		} else if (cell.getCellType() == CellType.NUMERIC) {
+			return String.valueOf((int) cell.getNumericCellValue());
+
+		}
+		return "";
+	}
+
+	// get total rows
+	public static int getRowCount() {
+		return sheet.getPhysicalNumberOfRows();
+	}
+
+	// close workbook
+
+	public static void closeExcel() throws IOException {
+		workbook.close();
+	}
+
+}
