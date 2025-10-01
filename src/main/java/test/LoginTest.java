0d22b9a723f51e38c2996f26565fe7937641fdde
siddheshparadkar83 <siddheshparadkar83@gmail.com>
2025-10-02 00:07:26 +0530

    suefire plugin

--- src/main/java/test/LoginTest.java
+++ src/main/java/test/LoginTest.java
@@ -37,7 +37,7 @@ public class LoginTest extends BaseTest {
 
 	@DataProvider(name = "LoginData2")
 	public Object[][] getData() {
-		return new Object[][] { { "user1", "pass1" }, { "user2", "pass2" }, { "user3", "pass3" }
+		return new Object[][] { { "user1", "pass1" }, { "user2", "pass2" }, { "admin@yourstore.com", "admin" }
 
 		};
 	}
